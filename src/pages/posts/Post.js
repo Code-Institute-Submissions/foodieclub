c392a94d7842dea47e6ed23a365554372d3aaf0d
Henry Leon <henryatusabes@gmail.com>
2023-03-31 22:37:46 +0000

    Add useAlert js in hooks directory

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -6,6 +6,7 @@ import { Link, useHistory } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
 import { MoreDropdown } from "../../components/MoreDropdown";
+// import IngredientsCreateForm from "../ingredients/IngredientsCreateForm";
 
 const Post = (props) => {
   const {
